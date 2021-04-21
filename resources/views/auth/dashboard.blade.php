698cc73ee4c4896490c40c7b12335963619e15f9
AlexAsole <alexgiorgio.asole@gmail.com>
2021-04-21 10:26:43 +0200

    fix

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -13,33 +13,39 @@
                             <div class="alert alert-success" role="alert">
                                 {{ session('status') }}
                             </div>
-                        @endif
-                        <strong style="font-size: 18px">{{ Auth::user()->name }} {{ Auth::user()->lastname }}</strong>
-                        <br>
-                        {{ __('You are logged in!') }} <br><br>
-                        Gestione dati utente registrato, rimanda all'edit <br>
-                        <button class="btn custom-button">
-                            <a href="{{ route('edit', ['user' => Auth::user()]) }}">
-                                Modifica i tuoi dati
-                            </a>
-                        </button>
-                        <button class="btn custom-button">
-                            <a href="{{ route('message', ['user' => Auth::user()]) }}">
-                                Visualizza messagi
-                            </a>
-                        </button>
-                        <form action="{{ route('destroy', ['user' => Auth::user()]) }}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-danger" type="submit">Elimina account</button>
-                        </form>
+                            {{-- @endif --}}
+                            <strong style="font-size: 18px">{{ Auth::user()->name }}
+                                {{ Auth::user()->lastname }}</strong>
+                            <br>
+                            {{ __('You are logged in!') }} <br><br>
+                            Gestione dati utente registrato, rimanda all'edit <br>
+                            <button class="btn custom-button">
+                                <a href="{{ route('edit', ['user' => Auth::user()]) }}">
+                                    Modifica i tuoi dati
+                                </a>
+                            </button>
+                            <button class="btn custom-button">
+                                <a href="{{ route('message', ['user' => Auth::user()]) }}">
+                                    Visualizza messagi
+                                </a>
+                            </button>
+                            <form action="{{ route('destroy', ['user' => Auth::user()]) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger" type="submit">Elimina account</button>
+                            </form>
                     </div>
                     @endif
-                    <img src="{{asset(Auth::user()->profile_image)}}">
-                    <strong style="font-size: 18px">{{ Auth::user()->name }} {{ Auth::user()->lastname }}</strong> <br>
+                    <strong style="font-size: 18px">{{ Auth::user()->name }} {{ Auth::user()->lastname }}</strong>
+                    <br>
                     {{ __('You are logged in!') }} <br><br>
                     Gestione dati utente registrato, rimanda all'edit <br>
-                    <a href="{{ route('edit', ['user' => Auth::user()]) }}">Modifica i tuoi dati</a>
+                    <button class="btn custom-button">
+                        <a href="{{ route('edit', ['user' => Auth::user()]) }}">Modifica i tuoi dati</a>
+                    </button>
+                    <button class="btn custom-button">
+                        <a href="{{ route('message', ['user' => Auth::user()]) }}">Visualizza i tuoi messaggi</a>
+                    </button>
                     <form action="{{ route('destroy', ['user' => Auth::user()]) }}" method="post">
                         @csrf
                         @method('DELETE')
