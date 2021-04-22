2467abab0eb366e7cd091c332779960d1d58d2e7
sabrina-fontana <73885980+sabrina-fontana@users.noreply.github.com>
2021-04-22 15:39:27 +0200

    correzione script

--- resources/views/auth/sponsorships.blade.php
+++ resources/views/auth/sponsorships.blade.php
@@ -1,8 +1,100 @@
-@extends('layouts.app')
-@section('title', 'Show')
-@section('content')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    @if (session('success_message'))
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>BDoctors - Sponsorizzazioni</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+        crossorigin="anonymous" />
+
+</head>
+
+<body>
+    <nav id="main-nav" class="navbar navbar-expand-md navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}" style="font-size: 30px">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHK4VZoPGy-_lCyOFjKY-FTDHq9B057V0Hj-tYYGYiL1r9MlTdLz409t0fp4AvQuciiDg&usqp=CAU"
+                    alt=""> BDoctors
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('create') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a
+                                id="dropp"
+                                class="dropdown-item"
+                                href="{{ route('dashboard') }}">
+                                    DashBoard
+                                </a>
+                                <a
+                                id="dropp"
+                                class="dropdown-item"
+                                href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="jum">
+        <img src="https://www.pngkit.com/png/full/254-2543504_doctors-getting-into-medical-school-the-premedical-students.png"
+            alt="">
+    </div>
+
+    <main class="py-4 container">
+        @if (session('success_message'))
         <div class="alert alert-success">
             {{ session('success_message') }}
         </div>
@@ -49,4 +141,48 @@
             <button class="button" type="submit"><span>Test Transaction</span></button>
         </form>
 
-@endsection
+    </main>
+
+    <footer>
+        <a href="{{ url('/') }}">
+            Powered with love by Team 2 | www.bdoctors.it
+        </a>
+    </footer>
+
+    <script src="https://js.braintreegateway.com/web/dropin/1.27.0/js/dropin.min.js"></script>
+    <script>
+        var form = document.querySelector('#payment-form');
+        var client_token = "{{$token}}";
+
+        braintree.dropin.create({
+        authorization: client_token,
+        selector: '#bt-dropin',
+        paypal: {
+            flow: 'vault'
+        }
+        }, function (createErr, instance) {
+        if (createErr) {
+            console.log('Create Error', createErr);
+            return;
+        }
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            instance.requestPaymentMethod(function (err, payload) {
+            if (err) {
+                console.log('Request Payment Method Error', err);
+                return;
+            }
+
+            // Add the nonce to the form and submit
+            document.querySelector('#nonce').value = 'fake-valid-visa-nonce';
+            console.log(document.querySelector('#nonce').value)
+            form.submit();
+            });
+        });
+        });
+    </script>    
+</body>
+
+</html>
+
