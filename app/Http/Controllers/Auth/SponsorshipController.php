94d7f2dc01fce262deefc2aaf04abe93c4c1196a
sabrina-fontana <73885980+sabrina-fontana@users.noreply.github.com>
2021-04-22 10:51:42 +0200

    inizio braintree

--- app/Http/Controllers/Auth/SponsorshipController.php
+++ app/Http/Controllers/Auth/SponsorshipController.php
@@ -6,13 +6,21 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
 use App\Sponsorship;
+use Braintree;
 
 class SponsorshipController extends Controller
 {
     public function buySponsorship(User $user)
     {
         $sponsorships = Sponsorship::all();
-        return view('auth.sponsorships', compact('user', 'sponsorships'));
-    }
+        $gateway = new Braintree\Gateway([
+            'environment' => config('services.braintree.environment'),
+            'merchantId' => config('services.braintree.merchantId'),
+            'publicKey' => config('services.braintree.publicKey'),
+            'privateKey' => config('services.braintree.privateKey'),
+        ]);
 
+        $token = $gateway->ClientToken()->generate();
+        return view('auth.sponsorships', ['user' => $user, 'sponsorships' => $sponsorships, 'token' => $token]);
+    }
 }
--- resources/views/auth/sponsorships.blade.php
+++ resources/views/auth/sponsorships.blade.php
@@ -12,4 +12,58 @@
         <hr>
         @endforeach
 
+        <form method="post" id="payment-form" action="#">
+            <section>
+                <label for="amount">
+                    <span class="input-label">Amount</span>
+                    <div class="input-wrapper amount-wrapper">
+                        <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="10">
+                    </div>
+                </label>
+
+                <div class="bt-drop-in-wrapper">
+                    <div id="bt-dropin"></div>
+                </div>
+            </section>
+
+            <input id="nonce" name="payment_method_nonce" type="hidden" />
+            <button class="button" type="submit"><span>Test Transaction</span></button>
+        </form>
+    </div>
+</div>
+
+<script src="https://js.braintreegateway.com/web/dropin/1.27.0/js/dropin.min.js"></script>
+<script>
+    var form = document.querySelector('#payment-form');
+    var client_token = "{{$token}}";
+
+    braintree.dropin.create({
+      authorization: client_token,
+      selector: '#bt-dropin',
+      paypal: {
+        flow: 'vault'
+      }
+    }, function (createErr, instance) {
+      if (createErr) {
+        console.log('Create Error', createErr);
+        return;
+      }
+      form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        instance.requestPaymentMethod(function (err, payload) {
+          if (err) {
+            console.log('Request Payment Method Error', err);
+            return;
+          }
+
+          // Add the nonce to the form and submit
+          document.querySelector('#nonce').value = payload.nonce;
+          form.submit();
+        });
+      });
+    });
+</script>
+
+
 @endsection
